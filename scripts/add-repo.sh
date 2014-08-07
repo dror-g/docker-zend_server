d8b810223e47099f9e8b7ceba3cad3320f227b1c
root <root@drorg.local>
2014-08-07 13:19:59 +0300

    Re-arranged file locations.

--- scripts/add-repo.sh
+++ scripts/add-repo.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#echo "deb http://repos.zend.com/zend-server/7.0/deb_ssl1.0 server non-free" >> /etc/apt/sources.list.d/zend-server.list
 echo "deb http://repos.zend.com/zend-server/7.0/deb_apache2.4 server non-free" >> /etc/apt/sources.list.d/zend-server.list
 wget -O - http://repos.zend.com/zend.key | apt-key add -
