841ea96b6f4680baaa90fe04e9357f4d3112134f
root <root@drorg.local>
2014-08-20 17:27:46 +0300

    Apply fix to 5.5 and cleanup.

--- scripts/add-repo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "deb http://repos.zend.com/zend-server/7.0/deb_apache2.4 server non-free" >> /etc/apt/sources.list.d/zend-server.list
-wget -O - http://repos.zend.com/zend.key | apt-key add -
